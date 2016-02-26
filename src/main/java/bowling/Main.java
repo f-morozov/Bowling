92acb0050ac868e59564b17c10ddfa0f9d4e5b8a
Fedor Morozov <f-morozov@ya.ru>
2016-02-27 01:07:28 +0300

    Handle Ctrl+C in game mode

--- src/main/java/bowling/Main.java
+++ src/main/java/bowling/Main.java
@@ -1,8 +1,14 @@
 package bowling;
 
+import java.util.NoSuchElementException;
+
 public class Main {
     public static void main(String[] args) {
         BowlingGame game = new BowlingGame(System.in, System.out);
-        game.runGame();
+        try {
+            game.runGame();
+        } catch(NoSuchElementException exception) {
+            System.out.println("\nInput closed.");
+        }
     }
 }
